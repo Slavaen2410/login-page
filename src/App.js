53577cc3d3647daa499b256c770b4191711cd81c
slavaen2410 <slavaen2410@gmail.com>
2024-12-03 09:48:59 +0500

    Обновление проекта

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ChatButton from "./components/ChatButton";
 import ChatWindow from "./components/ChatWindow";
-import ConnectionCounter from "./components/ConnectionCounter";
+
+function GoogleAnalytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Отправляем событие в GA при каждом изменении маршрута
+    window.gtag("config", "G-GTZSJGSMZW", {
+      page_path: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+}
 
 function App() {
-  const [isChatOpen, setChatOpen] = useState(false);
+  const [isChatOpen, setChatOpen] = React.useState(false);
 
   const toggleChat = () => setChatOpen(!isChatOpen);
 
   return (
     <Router>
+      <GoogleAnalytics />
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/profile/:userId" element={<div>Страница пользователя</div>} />
       </Routes>
       <ChatButton toggleChat={toggleChat} />
       {isChatOpen && <ChatWindow />}
-      <ConnectionCounter />
     </Router>
   );
 }
